cac5c410095d435a9c1f3b0e8e533d6460ce7541
huyteo <huyteo1003@gmail.com>
2026-04-27 11:06:04 +0700

    Completed multiply_strings

---
+++ Multiply_Strings/multiply_strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def multiply(self, num1, num2):
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        m, n = len(num1), len(num2)
+        result = [0] * (m + n)
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = int(num1[i]) * int(num2[j])
+                p1, p2 = i + j, i + j + 1
+
+                total = mul + result[p2]
+                result[p2] = total % 10
+                result[p1] += total // 10
+
+        return ''.join(map(str, result)).lstrip('0')
+
+
+if __name__ == "__main__":
+    num1 = "123"
+    num2 = "456"
+
+    sol = Solution()
+    print(sol.multiply(num1, num2))
