48fb294d39a258b5d341ff549b356ab14c4d9f76
huyteo <huyteo1003@gmail.com>
2026-04-27 16:24:15 +0700

    Completed n-queens

---
+++ N-Queens/n-queens.py
@@ -0,0 +1,45 @@
+class Solution:
+    def solveNQueens(self, n):
+        result = []
+        board = [["."] * n for _ in range(n)]
+
+        cols = set()
+        diag1 = set()
+        diag2 = set()
+
+        def backtrack(r):
+            if r == n:
+                result.append(["".join(row) for row in board])
+                return
+
+            for c in range(n):
+                if c in cols or (r - c) in diag1 or (r + c) in diag2:
+                    continue
+
+                board[r][c] = "Q"
+                cols.add(c)
+                diag1.add(r - c)
+                diag2.add(r + c)
+
+                backtrack(r + 1)
+
+                board[r][c] = "."
+                cols.remove(c)
+                diag1.remove(r - c)
+                diag2.remove(r + c)
+
+        backtrack(0)
+        return result
+
+
+if __name__ == "__main__":
+    n = 4
+
+    sol = Solution()
+    solutions = sol.solveNQueens(n)
+
+    print("Số nghiệm:", len(solutions))
+    for s in solutions:
+        print()
+        for row in s:
+            print(row)
