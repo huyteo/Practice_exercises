69fcadd41872d9b1f91fe843f5d2d0b9ffb40d6a
huyteo <huyteo1003@gmail.com>
2026-04-27 10:21:13 +0700

    Completed comination_sum_2

---
+++ Combination_Sum_II/combination_sum_2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        result = []
+
+        def backtrack(start, path, total):
+            if total == target:
+                result.append(path[:])
+                return
+
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                path.append(candidates[i])
+                backtrack(i + 1, path, total + candidates[i])
+                path.pop()
+
+        backtrack(0, [], 0)
+        return result
+
+
+if __name__ == "__main__":
+    candidates = [10,1,2,7,6,1,5]
+    target = 8
+
+    sol = Solution()
+    print(sol.combinationSum2(candidates, target))
